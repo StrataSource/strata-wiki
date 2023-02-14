12f5fc4a45ece7e81c573839013885e8128812e5
tsa96 <tom@trumpetdust.org>
2023-02-14 15:58:45 +0000

    feat: Add ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        project: 'tsconfig.json',
+        sourceType: 'default'
+    },
+    plugins: ['@typescript-eslint/eslint-plugin', 'unicorn', 'unused-imports', 'prettier'],
+    extends: ['plugin:@typescript-eslint/recommended', 'plugin:unicorn/recommended', 'prettier'],
+    root: true,
+    env: {
+        node: true,
+        es2022: true
+    },
+    ignorePatterns: ['.eslintrc.js', 'node_modules', '_*.ts', 'build', '**/*.d.ts'],
+    rules: {
+        '@typescript-eslint/interface-name-prefix': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'unused-imports/no-unused-imports': 'error',
+        'unused-imports/no-unused-vars': [
+            'warn',
+            {
+                vars: 'all',
+                args: 'after-used',
+                varsIgnorePattern: '^_',
+                argsIgnorePattern: '^_'
+            }
+        ],
+        '@typescript-eslint/no-inferrable-types': ['warn', { ignoreParameters: true }],
+        // Way too sensitive. Most cases it catches are silly, and bad naming is easy to flag in review.
+        'unicorn/prevent-abbreviations': ['off'],
+        // Not going to make devs use obscure JS syntax for something so minor.
+        'unicorn/numeric-separators-style': ['warn', { onlyIfContainsSeparator: true }],
+        // Removing `null` entirely is a noble intention, but reality is that many libraries use it explicitly,
+        // especially Prisma. Plus, it's an further obscure thing for new devs to learn about. Better to flag bad uses
+        // in review, where the distinction can be explained.
+        'unicorn/no-null': ['off'],
+        // We're still on CJS for now, not bothering with path aliasing on ES modules yet. Can remove if we switch to ES.
+        'unicorn/prefer-module': ['off'],
+        // Same as above.
+        'unicorn/prefer-top-level-await': ['off'],
+        // Even though unicorn has a rule for no nested ternaries, it's insisting I make them due to this rule.
+        'unicorn/prefer-ternary': ['error', 'only-single-line'],
+        // Better parity with other languages, we use `1 << 0` frequently next to other shifts when defining bitflags.
+        'unicorn/prefer-math-trunc': ['off'],
+        // Why???
+        'unicorn/switch-case-braces': ['off']
+    }
+};
