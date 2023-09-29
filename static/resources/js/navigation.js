06fad51d7a310f5a14394484efbaf208f77289f7
JoLoZ <ev3mind@gmail.com>
2023-09-29 14:29:51 +0200

    feat: Special Momentum treatment

--- static/resources/js/navigation.js
+++ static/resources/js/navigation.js
@@ -143,6 +143,8 @@ async function navigate(slug, replace = false, loadData = true) {
     document.title = `${data.title || 'Page not found'} - ${games[info.game].name} Wiki`;
     document.querySelector('#current-game').innerText = games[info.game].name;
 
+    document.querySelector(".game h2").innerText = info.game == "momentum" ? "Engine Reference" : "Documentation";
+
     document.querySelector('link[rel=icon]').href = games[info.game].favicon || games[info.game].icon;
     document.querySelector('link[rel=shortcut]').href = games[info.game].favicon || games[info.game].icon;
 
