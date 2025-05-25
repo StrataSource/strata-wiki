00d0b60923d6b5d9dabeb23f45d9f58bea32a15e
JoLoZ <ev3mind@gmail.com>
2025-05-25 02:02:12 +0200

    Run updates

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
-import pagefind from "vite-plugin-pagefind";
+import { pagefind } from "vite-plugin-pagefind";
 
 export default defineConfig({
+    //@ts-ignore
     plugins: [pagefind(), sveltekit()],
     css: {
         preprocessorOptions: {
