93edae202b8de242851060cd17a500357b252d5c
JoLoZ <ev3mind@gmail.com>
2025-05-31 21:53:26 +0200

    Made Search bar auto focus

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,15 @@ import { defineConfig } from "vite";
 import { pagefind } from "vite-plugin-pagefind";
 
 export default defineConfig({
-    //@ts-ignore
-    plugins: [pagefind(), sveltekit()],
+    plugins: [
+        //@ts-expect-error They haven't updated their types yet
+        pagefind({
+            outputDirectory: "build",
+            assetsDirectory: "static",
+            developStrategy: "lazy",
+        }),
+        sveltekit(),
+    ],
     css: {
         preprocessorOptions: {
             scss: {
